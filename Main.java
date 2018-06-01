7415968b829c1e7f403d289f550a3d2e7ca34630
kmichy <aaaa0424pg@gmail.com>
2018-06-01 16:59:35 +0900

    1-3

--- Main.java
+++ Main.java
@@ -4,7 +4,20 @@ public class Main{
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         String a = sc.next();
-        a = a.replace("ta","");
-        System.out.println(a);
+        int b = sc.nextInt();
+        int c = a.length();
+        int d = b-c;
+        int e = d/2;
+        for(int i = 0;i <e;i++){
+            System.out.print("_");
+        }
+        System.out.print(a);
+        for(int i = 0;i <e;i++){
+            System.out.print("_");
+        }
+        if(d % 2 == 1){
+            System.out.print("_");
+        }
+        System.out.println();
     }
 }
